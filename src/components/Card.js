26ee68cb451b0c7ba0c24058bf73808b07a1ddd0
Damian <damiandanielvigo@gmail.com>
2021-03-08 09:04:54 -0300

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+      <div className="card-body">
+      <img src="" alt=""/>
+        <h4 className='card-title'>My title</h4>
+        <p className='card-text text-secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias officia, vero sint quo eveniet qui perspiciatis deleniti eius nemo nostrum? Alias numquam temporibus, laudantium optio pariatur itaque enim dolorem! Nihil.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
